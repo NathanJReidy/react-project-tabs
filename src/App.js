52cc504768af03b6fce5920e52a0770a0d0ef8bd
Nathan Reidy <nathanjreidy@gmail.com>
2021-05-04 20:32:11 +1000

    More design changes

--- src/App.js
+++ src/App.js
@@ -7,18 +7,24 @@ function App() {
   return (
     <main>
       <section>
-        <h2 className="title">Experience</h2>
+        <div className="title">
+          <h2>Experience</h2>
+          <div className="underline"></div>
+        </div>
+
         <div className="container">
           <div className="tabsContainer">
-            <h3 className="tabsName">Tommy</h3>
+            <button className="tabsNameButton">
+              <h3 className="tabsName">Tommy</h3>
+            </button>
           </div>
           <div className="resume">
             <h3>Job Title</h3>
             <h4>Name Flair</h4>
             <h4>May 2015 - December 2015</h4>
             <div className="dutiesContainer">
-              <FaAngleDoubleRight />
-              <p>
+              <FaAngleDoubleRight size={48} style={{ fill: "#2caeba" }} />
+              <p className="duties">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed
                 soluta itaque quaerat voluptate vitae quod natus repudiandae in
                 dignissimos error dolorem sit debitis, officiis iste dolore
